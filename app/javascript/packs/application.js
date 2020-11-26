927275bd3f675161e9a700c20079c728bcad2c29
Nicolas Enne <nicolaseenne@gmail.com>
2020-11-26 18:22:15 -0300

    changed stars review from six to five

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,12 +26,11 @@ require("channels")
 import "bootstrap";
 import { initStarRating } from '../plugins/init_star_rating';
 
-initStarRating();
-
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initStarRating();
 });
